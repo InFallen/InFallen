8282eb80f54f08ca79ee8835e0df0d07a1e5d45b
InFallen <infallen1090@yandex.ru>
2022-06-29 13:36:17 +0300

    change struct page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 const list = document.querySelectorAll('.list');
+const parts = document.querySelectorAll('.part');
 function activeLink(){
-    list.forEach((item) => 
-    item.classList.remove('active'));
+    list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
+    var link = this.querySelectorAll('a')[0].href;
+    
+    parts.forEach((part) => activePage(part, link));
 }
 list.forEach((item) =>
 item.addEventListener('click', activeLink));
+
+function activePage(item, link) {
+        item.classList.remove('active');
+        var name = item.getAttribute('name');
+        console.log(link);
+        if(link.includes(name)){
+            item.classList.add('active');
+        }
+};
