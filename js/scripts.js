9d31f2991947b15f0f67b5d65a14eb9440867a67
InFallen <infallen1090@yandex.ru>
2022-06-30 15:05:07 +0300

    change style

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 const list = document.querySelectorAll('.list');
 const parts = document.querySelectorAll('.part');
+var title = document.title;
 function activeLink(){
     list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
     var link = this.querySelectorAll('a')[0].href;
+    parts.forEach((part) => activePage(part, link, this));
     
-    parts.forEach((part) => activePage(part, link));
 }
 list.forEach((item) =>
 item.addEventListener('click', activeLink));
 
-function activePage(item, link) {
+function activePage(item, link, list) {
         item.classList.remove('active');
         var name = item.getAttribute('name');
-        console.log(link);
         if(link.includes(name)){
             item.classList.add('active');
+            document.title = title + ' ' + name.toUpperCase();
         }
+        
 };
