22bac18927a31bcb6749512c0ca32d05dc8eed1c
InFallen <infallen1090@yandex.ru>
2022-07-11 15:54:02 +0300

    formating code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 const list = document.querySelectorAll('.list');
 const parts = document.querySelectorAll('.part');
 var title = document.title;
-function activeLink(){
+function activeLink() {
     list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
     var link = this.querySelectorAll('a')[0].href;
     parts.forEach((part) => activePage(part, link, this));
-    
+
 }
 list.forEach((item) =>
-item.addEventListener('click', activeLink));
+    item.addEventListener('click', activeLink));
 
 function activePage(item, link, list) {
-        item.classList.remove('active');
-        var name = item.getAttribute('name');
-        if(link.includes(name)){
-            item.classList.add('active');
-            document.title = title + ' ' + name.toUpperCase();
-        }
-        
+    item.classList.remove('active');
+    var name = item.getAttribute('name');
+    if (link.includes(name)) {
+        item.classList.add('active');
+        document.title = title + ' ' + name.toUpperCase();
+    }
+
 };
