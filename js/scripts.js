acd89a6fcb56da26451ce92b48ebfbc52c15f1f5
InFallen <infallen1090@yandex.ru>
2022-07-18 13:37:33 +0300

    Обновление страницы, сохраняет открытую вкладку меню.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 const list = document.querySelectorAll('.list');
 const parts = document.querySelectorAll('.part');
 var title = document.title;
+
+
+
 function activeLink() {
     list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
     var link = this.querySelectorAll('a')[0].href;
     parts.forEach((part) => activePage(part, link, this));
-
 }
-list.forEach((item) =>
-    item.addEventListener('click', activeLink));
+
+
 
 function activePage(item, link, list) {
     item.classList.remove('active');
@@ -20,3 +22,40 @@ function activePage(item, link, list) {
     }
 
 };
+
+function loadPage() {
+    var link = String(document.location);
+    if (!link.includes('index')) {
+        return;
+    }
+    var startIndex = link.lastIndexOf('#') + 1;
+    if (startIndex === 0) {
+        return;
+    }
+    var endIndex = link.length;
+    var part = link.substring(startIndex, endIndex);
+    list.forEach((item) => {
+        item.classList.remove('active');
+        var href = item.querySelectorAll('a')[0].href;
+        if (href.includes(part)) {
+            item.classList.add('active');
+        }
+        parts.forEach((item) => {
+            item.classList.remove('active');
+            var name = item.getAttribute('name')
+            if (name === part) {
+                item.classList.add('active');
+                document.title = title + ' ' + name.toUpperCase();
+            }
+        });
+    });
+}
+
+// window.onload = loadPage;
+window.addEventListener('load', loadPage);
+
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+
+
